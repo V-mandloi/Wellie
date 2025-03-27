30f198924e2189fbfc368a311fdc52949105a0e9
swati <swati09pal07@gmail.com>
2025-03-27 14:19:29 +0530

    footer created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/footer/footer";
 import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Footer/>
     </>
   );
 }
