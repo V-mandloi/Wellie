21f13da036d97094aebc63e6951806f6dd74444b
Vikram Mandloi <vmandloi852@gmail.com>
2025-03-27 12:08:12 +0530

    initial commit

--- Frontend/src/components/navbar/navbar.jsx
+++ Frontend/src/components/navbar/navbar.jsx
@@ -44,25 +44,6 @@ export default function Example() {
                 className="h-8 w-auto"
               />
             </div>
-            {/* <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? "page" : undefined}
-                    className={classNames(
-                      item.current
-                        ? "bg-gray-900 text-white"
-                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                      "rounded-md px-3 py-2 text-sm font-medium"
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </div> */}
           </div>
           {/* Moved navbar items to replace notification and profile section */}
           <div className="hidden  sm:flex sm:space-x-4">
