becbdfb27f1f2ea1d8ea6592114a2603c274ce67
swati <swati09pal07@gmail.com>
2025-04-28 16:51:48 +0530

    cards size done

--- Frontend/src/components/About/about6.jsx
+++ Frontend/src/components/About/about6.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-
-const Card = ({ title, description, image }) => {  
+const Card = ({ title, description, image }) => {
   return (
-    <div className="bg-secondary rounded-lg shadow-lg p-6 m-4 text-center transition-transform duration-300 transform hover:scale-105 cursor-pointer hover:bg-stone-100">
+    <div className="bg-secondary rounded-lg shadow-lg p-6 m-2 text-center transition-transform duration-300 transform hover:scale-105 cursor-pointer hover:bg-stone-100">
       <div className="mb-4">
-        <img 
-          src={image}  
-          alt={title} 
-          className="w-20 h-20 object-cover mx-auto" 
+        <img
+          src={image}
+          alt={title}
+          className="w-40 h-30 object-cover mx-auto"
         />
       </div>
       <h2 className="text-xl font-bold mb-2">{title}</h2>
@@ -15,5 +14,4 @@ const Card = ({ title, description, image }) => {
     </div>
   );
 };
-
 export default Card ;
